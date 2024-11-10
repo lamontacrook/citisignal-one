ee1ca2ed281963311007195e0f9d055f4d46f34e
Lamont Crook <lamont@adobe.com>
2024-11-10 13:21:50 -0700

    update cards.js

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,7 +1,8 @@
-import { createOptimizedPicture } from '../../scripts/aem.js';
+import { createOptimizedPicture } from '../../scripts/scripts.js';
 import { moveInstrumentation } from '../../scripts/scripts.js';
 
 export default function decorate(block) {
+  console.log(block);
   /* change to ul, li */
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
