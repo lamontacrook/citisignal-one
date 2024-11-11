6869279cce968068d3b68e10ad963ebbf630acb6
Lamont Crook <lamont@adobe.com>
2024-11-11 10:25:19 -0800

    update script.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -331,7 +331,8 @@ function decorateButtons(main) {
     const altText = 'my alt';
     const block = whatBlockIsThis(a)
     const imgName = deliveryUrl.substring(deliveryUrl.lastIndexOf('/') + 1);
-    const bp = getMetadata(block);
+    // const bp = getMetadata(block);
+    const bp = getMetadata('columns');
     let breakpoints = [{ media: '(min-width: 600px)', width: '2000' }, { width: '750' }];
 
     if (bp) {
